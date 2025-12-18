3e0f8abac8bdcac2711b50c11cc2b7151d714d9e
Aatif Ahmad <ahmadaatif6@gmail.com>
2025-12-18 17:04:45 +0000

    change the footer color

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import { Code, Phone, MapPin, Mail } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="bg-card">
+    <footer className="bg-accent">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
           <div className="flex flex-col items-center md:items-start">
